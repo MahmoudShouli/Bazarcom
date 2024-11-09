2449081a75174af717401d2ce62fdf1b043d773a
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2024-11-09 16:28:14 +0200

    added in-memory caching for search queries using NodeCache module

--- front-end-service/server.js
+++ front-end-service/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const NodeCache = require('node-cache');
 const { responseTimeMiddleware } = require('./middleware/responseTime');
 
 const app = express();
 const PORT = process.env.PORT || 3002;
+
+const cache = new NodeCache({ stdTTL: 3600, checkperiod: 120 });
 
 app.use(bodyParser.json());
 app.use(responseTimeMiddleware);
@@ -18,8 +21,20 @@ app.get('/', (req, res) => {
 
 // Endpoint to search by topic
 app.get('/search/:topic', async (req, res) => {
+
+    const topic = req.params.topic;
+
+    const cachedData = cache.get(topic);
+
+    if(cachedData){
+        console.log("Cache hit for topic : ", topic)
+        return res.json(cachedData);
+    }
+
     try {
-        const response = await axios.get(`http://catalog-service:3000/search/${req.params.topic}`);
+        const response = await axios.get(`http://catalog-service:3000/search/${topic}`);
+        cache.set(topic, response.data);
+        console.log("Cache miss for topic : ", topic)
         res.json(response.data);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching data from catalog service' });
@@ -28,15 +43,28 @@ app.get('/search/:topic', async (req, res) => {
 
 // Endpoint to get info by item ID
 app.get('/info/:id', async (req, res) => {
+
+    const id = req.params.id;
+
+    const cachedData = cache.get(id);
+
+    if(cachedData){
+        console.log("Cache hit for book with id : ", id)
+        return res.json(cachedData);
+    }
+
+
     try {
-        const response = await axios.get(`http://catalog-service:3000/info/${req.params.id}`);
+        const response = await axios.get(`http://catalog-service:3000/info/${id}`);
+        cache.set(id, response.data);
+        console.log("Cache miss for book with id : ", id)
         res.json(response.data);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching data from catalog service' });
     }
 });
 
-// Endpoint to purchase by item ID
+// Endpoint to purchase by item ID (no cache, because it's a post request)
 app.post('/purchase/:id', async (req, res) => {
     const { quantity } = req.body;
     try {
