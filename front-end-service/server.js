7a614ee999be55956a0c2511bb83108ecbf36367
Nabeel <xnabeel972@gmail.com>
2024-11-10 16:20:59 +0200

    Added replication: - added some functionality to the front end service server to ensure the load balancing - make some change on the .yml file (deploy)

--- front-end-service/server.js
+++ front-end-service/server.js
@@ -14,6 +14,26 @@ app.use(bodyParser.json());
 app.use(responseTimeMiddleware);
 app.use(express.static(path.join(__dirname, 'views')));
 
+// for load balancing between replicas
+let catalogServers = ['catalog-service_1:3000', 'catalog-service_2:3000'];
+let orderServers = ['order-service_1:3001', 'order-service_2:3001'];
+
+let catalogIndex = 0;
+let orderIndex = 0;
+
+function getNextCatalogServer() {
+    const server = catalogServers[catalogIndex];
+    catalogIndex = (catalogIndex + 1) % catalogServers.length; // Round-robin
+    return server;
+}
+
+function getNextOrderServer() {
+    const server = orderServers[orderIndex];
+    orderIndex = (orderIndex + 1) % orderServers.length; // Round-robin
+    return server;
+}
+
+
 // Endpoint to serve the HTML page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
@@ -32,9 +52,11 @@ app.get('/search/:topic', async (req, res) => {
     }
 
     try {
-        const response = await axios.get(`http://catalog-service:3000/search/${topic}`);
+        const catalogServer = getNextCatalogServer();
+        const response = await axios.get(`http://${catalogServer}:3000/search/${topic}`);
         cache.set(topic, response.data);
         console.log("Cache miss for topic : ", topic)
+        console.log("response returned from : ", catalogServer)
         res.json(response.data);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching data from catalog service' });
@@ -55,9 +77,11 @@ app.get('/info/:id', async (req, res) => {
 
 
     try {
-        const response = await axios.get(`http://catalog-service:3000/info/${id}`);
+        const catalogServer = getNextCatalogServer();
+        const response = await axios.get(`http://${catalogServer}:3000/info/${id}`);
         cache.set(id, response.data);
         console.log("Cache miss for book with id : ", id)
+        console.log("response returned from : ", catalogServer)
         res.json(response.data);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching data from catalog service' });
@@ -68,8 +92,10 @@ app.get('/info/:id', async (req, res) => {
 app.post('/purchase/:id', async (req, res) => {
     const { quantity } = req.body;
     try {
-        const response = await axios.post(`http://order-service:3001/purchase/${req.params.id}`, { quantity });
+        const orderServer = getNextOrderServer();
+        const response = await axios.post(`http://${orderServer}:3001/purchase/${req.params.id}`, { quantity });
         res.json(response.data);
+        console.log("response returned from : ", orderServer)
     } catch (error) {
         res.status(500).json({ error: 'Error purchasing item' });
     }
